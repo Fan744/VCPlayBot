f288d2f2fd37cb56baff45d494e0cf1c64e62b8c
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-04-19 12:40:43 +0530

    Update str.py

--- str.py
+++ str.py
@@ -9,7 +9,7 @@ async def main():
     async with Client(
         ":memory:", api_id=int(input("28962746:")), api_hash=input("727457f88d661b08e636188a949cd9f3:")
     ) as app:
-        print(await app.export_session_string())
+        print(await app.export_session_string(BQG577oAilvG1uSvyOXMaelV0o-CvAEmnP-5fY-oAdpJZRT620a0jAM7fbT0_By5qrxv7pLgOqCL-BFJlpN0P1qPCt9gvfGBO1po1qTIp4w-MuHTu2PIfG76DEOorT9YE5XYDzbJ8j6EjXLWrdpJ0wdKX-YcQypt4n9Sf_zLfrzN2D6LCP50wINvGVMLOGwUjos3tOPSY4nPeZWbLnc6UAlByHv-i5_xJTIyyb56keQD2wGPa2DgyME6MvPs6PSoFs7jyOW6mXuA1Ou5yL4FaFhDRvwr80XbTkDtwHSmWVys1Trk8Y4agToQ-hIbyP8lB2OAJ-Vu_eQb7vxVV_Q70Jo9zGgb8wAAAAGu00bKAA))
 
 
 if __name__ == "__main__":
